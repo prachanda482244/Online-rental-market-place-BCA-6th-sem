d578d72f41c8abf29c126fa33d426e4a83370c31
Prachanda Rana <62231260+prachanda482244@users.noreply.github.com>
2023-12-28 20:53:05 +0545

    Install cors and setup cors in express

--- api/index.js
+++ api/index.js
@@ -4,9 +4,10 @@ import { connectToDb } from "./src/db/connect.js";
 import userRouter from "./src/routers/user.route.js";
 import UserRouter from "./src/routers/auth.route.js";
 import { errorMiddleware } from "./src/middleware/error.middleware.js";
-
+import cookieParser from "cookie-parser";
+import cors from 'cors'
 const app = express()
-app.use(express.json())
+app.use(express.json({ limit: '16kb' }))
 connectToDb();
 app.get('/', (req, res) => {
     res.send("hello world")
@@ -15,6 +16,13 @@ app.get('/', (req, res) => {
 app.use(userRouter)
 app.use(UserRouter)
 app.use(errorMiddleware)
+app.use(cors({
+    origin: process.env.CORS_ORIGIN,
+    credentials: true
+}))
+app.use(express.urlencoded({ extended: true, limit: '16kb' }))
+app.use(cookieParser())
+
 app.listen(port, () => {
     console.log(`Server running on  port ${port}`)
 })
